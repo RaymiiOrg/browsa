1c06dd3819a749064bfff7eb2c3c0eaa471bc4cc
Remy van Elst <relst@relst.nl>
2013-07-04 22:45:40 +0200

    Better error handling and more random fallback urls

--- browsa.py
+++ browsa.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import sys, urllib2, os, re, random
 
-fallback_url = "http://news.google.com"
+fallback_urls = [u"https://news.ycombinator.com/", u"http://news.google.com", u"http://raymii.org/s/tags", 
+                 u"http://tweakers.net/nieuws", u"http://stackoverflow.com", u"http://reddit.com"]
+fallback_url = random.choice(fallback_urls)
 
 try:
-    page_url = sys.argv[1]
+    page_url = u"".join(sys.argv[1])
 except IndexError:
     sys.exit("Please provide an URL")
 
 urls = []
+non_wanted_urls = ['.pdf', '@', '#', '.js']
+
+print(page_url)
 
 page_request = urllib2.Request(page_url)
 fallback_request = urllib2.Request(fallback_url)
@@ -18,35 +24,38 @@ page_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windo
 fallback_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)')
 page_request.add_header('Accept', 'text/plain')
 fallback_request.add_header('Accept', 'text/plain')
-  
+page_request.add_header('Accept-Encoding', 'none')
+fallback_request.add_header('Accept-Encoding', 'none')
+
 try:
     page = urllib2.urlopen(page_request).read()
 except Exception:
     page = urllib2.urlopen(fallback_request).read()
     print("---")
+
 soup = BeautifulSoup(page)
+
 for link in soup.find_all('a'):
-    if re.search("htt(p|ps)\:\/\/", str(link.get('href'))):
-        # print(link.get('href'))
-        urls.append(link.get('href'))
-    else:
-        if link.get('href'):
-            # print(str(page_url) + str(link.get('href')))
-            urls.append(str(page_url) + str(link.get('href')))
+    if link.get('href'):
+        if re.search("htt(p|ps)\:\/\/", link.get('href').encode('utf-8')):
+            urls.append(link.get('href'))
+
 if len(urls) == 0:
     print("----")
     urls.append(fallback_url)
+
 url = random.choice(urls)
 
-if url == page_url:
-    sys.exit()
-if re.search("@", url) or re.search("#", url):
-    sys.exit()
+if url == page_url and not page_url == fallback_url:
+    url = fallback_url
 
-scriptname = sys.argv[0]
+for item in non_wanted_urls:
+    if re.search(item, url):
+        url = fallback_url
+
+scriptname = u''.join(sys.argv[0])
 
 command = scriptname + " '" + url + "' &"
-print(url)
 
 try:
     os.system(command)
