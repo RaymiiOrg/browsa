1dd0339ea02570f40d57d4b9db485c840a7a452f
Remy <relst@relst.nl>
2014-09-02 14:03:51 +0200

    Rewrote to be cleaner and work without forking

--- browsa.py
+++ browsa.py
@@ -5,63 +5,115 @@ import sys, urllib2, os, re, random
 
 fallback_urls = [u"https://news.ycombinator.com/", u"http://news.google.com", u"http://raymii.org/s/tags", 
                  u"http://tweakers.net/nieuws", u"http://stackoverflow.com", u"http://reddit.com"]
-fallback_url = random.choice(fallback_urls)
 
-try:
-    page_url = u"".join(sys.argv[1])
-except IndexError:
-    sys.exit("Please provide an URL")
+def get_url(page_url) :
 
-urls = []
-non_wanted_urls = ['.pdf', '@', '#', '.js', '.jpg', '.png', '.gif']
+    global fallback_urls
+    fallback_url = random.choice(fallback_urls)
 
-print(page_url)
+    print("URL: " + page_url)
+    
+    page_request = urllib2.Request(page_url)
+    fallback_request = urllib2.Request(fallback_url)
+     
+    page_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)')
+    fallback_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)')
+    page_request.add_header('Accept', 'text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+    fallback_request.add_header('Accept', 'text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+    page_request.add_header('Accept-Encoding', 'none')
+    fallback_request.add_header('Accept-Encoding', 'none')
+    page_request.add_header('DNT', '1')
+    fallback_request.add_header('DNT', '1')
+    page_request.add_header('Connection', 'keep-alive')
+    fallback_request.add_header('Connection', 'keep-alive')
+    
+    try:
+        page = urllib2.urlopen(page_request).read()
+        fallback_urls.append(page_url)
+    except Exception as error:
+        try:
+            page = urllib2.urlopen(fallback_request).read()
+            print("# Error in opening URL: %s. Falling back to fallback url: %s" % (error, fallback_url))
+        except Exception as error:
+            print("# Fallback URL %s also failed with error: %s. Giving up." % (fallback_url, error))
+            print("# Fallback URL's collected: ")
+            for url in fallback_urls:
+                print("# %s" % url)
+            print("")
+            sys.exit(1)
 
-page_request = urllib2.Request(page_url)
-fallback_request = urllib2.Request(fallback_url)
- 
-page_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)')
-fallback_request.add_header('User-agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)')
-page_request.add_header('Accept', 'text/plain')
-fallback_request.add_header('Accept', 'text/plain')
-page_request.add_header('Accept-Encoding', 'none')
-fallback_request.add_header('Accept-Encoding', 'none')
+    soup = BeautifulSoup(page)
+    return soup
+    
+def get_all_links_from_soup(soup):
+    urls = []
+    for link in soup.find_all('a'):
+        if link.get('href'):
+            if re.search("htt(p|ps)\:\/\/", link.get('href').encode('utf-8')):
+                urls.append(link.get('href'))
+    print("%i URL's on page" % len(urls))
+    return urls
 
-try:
-    page = urllib2.urlopen(page_request).read()
-except Exception as error:
-    page = urllib2.urlopen(fallback_request).read()
-    print("# Error in opening URL: %s" % error)
-
-soup = BeautifulSoup(page)
-
-for link in soup.find_all('a'):
-    if link.get('href'):
-        if re.search("htt(p|ps)\:\/\/", link.get('href').encode('utf-8')):
-            urls.append(link.get('href'))
-
-if len(urls) == 0:
-    print("# Error: No URLs in list.")
-    urls.append(fallback_url)
-
-url = random.choice(urls)
-
-if url == page_url and not page_url == fallback_url:
-    url = fallback_url
-
-for item in non_wanted_urls:
-    if re.search(item, url):
-        url = fallback_url
-
-scriptname = u''.join(sys.argv[0])
-
-command = scriptname + " '" + url + "' &"
-
-try:
-    os.system(command)
-except Exception as error:
+def choose_new_url(urls, previous_url):
+    global fallback_urls
+    fallback_url = random.choice(fallback_urls)
+    non_wanted_urls = ['.pdf', '@', '#', '.js', '.jpg', '.png', '.gif']
+    
+    if len(urls) == 0:
+        print("# Error: No URLs in list.")
+        urls.append(fallback_url)
+     
     url = random.choice(urls)
-    command = scriptname + " '" + url + "' &"
-    os.system(command)
+    print("Contemplating random URL: %s" % url) 
+    
+    if url == previous_url:
+        print("# New URL Would be the same as previous URL")
+        if not fallback_url == previous_url:
+            print("# Choosing a fallback URL.")
+            url = fallback_url
+        else:
+            print("# Previous URL is also the fallback URL.")
+            url = fallback_url
 
-sys.exit()
+    
+    for item in non_wanted_urls:
+        if re.search(item, url):
+            print("# Part of the new URL is on the non wanted list. Choosing fallback URL.")
+            url = fallback_url
+
+    print("New URL: %s" % url) 
+    return url
+
+
+def launch_myself_in_background(url):
+    scriptname = u''.join(sys.argv[0])
+    
+    command = scriptname + " '" + url + "' &"
+    
+    try:
+        os.system(command)
+    except Exception as error:
+        url = random.choice(urls)
+        command = scriptname + " '" + url + "' &"
+        os.system(command)
+    
+    sys.exit()
+
+def the_magic(url):
+    soup = get_url(url)
+    urls = get_all_links_from_soup(soup)
+    new_url = choose_new_url(urls, url)
+    return new_url
+
+def main():
+    try:
+        page_url = u"".join(sys.argv[1])
+    except IndexError:
+        sys.exit("Please provide an URL")
+
+    while True:
+        page_url = the_magic(page_url)
+    print("")
+    
+if __name__ == "__main__":
+    main()
